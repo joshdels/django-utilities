04d5e0d369fc8b9891f158f0631db81bbfd60051
joshdels <deleonjoshy2@gmail.com>
2026-04-12 16:53:02 +0800

    chore: endpoint of count

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import process_dxf, task_status, download_file
+from .views import process_dxf, task_status, download_file, upload_stats
 
 urlpatterns = [
     path("process-dxf/", process_dxf),
     path("task-status/<str:task_id>/", task_status),
     path("download/<str:task_id>/", download_file),
+    path("file-storage-count/", upload_stats),
 ]
